80a7c63926883a63358d5160a42e99e50524e622
Anderson <anderson.cardenas.r@gmail.com>
2022-04-04 11:09:44 -0500

    Hasta tener too configurado

--- BlogPersonal/BlogPersonal/urls.py
+++ BlogPersonal/BlogPersonal/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Apps.Publicaciones.urls'))
+    # todos los urls de las aplicaciones
 ]
